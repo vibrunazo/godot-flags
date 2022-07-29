c63e61228dd71d8912409bc9a2a1f1a6b8c01a3a
Vandré Brunazo <vibrunazo@gmail.com>
2022-07-29 18:54:34 -0300

    editor tool gets countries from global

--- scenes/Flag.gd
+++ scenes/Flag.gd
@@ -13,22 +13,9 @@ func _ready():
 
 func _process(delta):
 	if Engine.editor_hint:
-		# Code to execute in editor.
-#		print("flag in editor, country is {country}".format({"country": country}))
-		if country == "brazil":
-			$Img.texture = load("res://tex/brazil.png")
-		if country == "japan":
-			$Img.texture = load("res://tex/japan.png")
-
-	if not Engine.editor_hint:
-		# Code to execute in game.
-		pass
+		update_tex()
 
 func update_tex():
-	self.country = "germany"
-	$Img.texture = load("res://tex/germany.png")
-	print('updated texture')
-	pass
-
-func set_country(new_country):
-	print("setted a country to ")
+	if Global.flags.has(country):
+		var path = "res://tex/%s.png" % country
+		$Img.texture = load(path)
---
+++ scenes/Global.gd
@@ -0,0 +1,22 @@
+tool
+
+extends Node
+
+var flags = {
+	"brazil": {},
+	"canada": {},
+	"germany": {},
+	"japan": {}
+}
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+func get_flag(name):
+	return flags[name]
